946ddcf2c76049ce049ed0b0c9eca130cdbb08e2
Salim Ramirez <salimramirezm@gmail.com>
2026-01-01 19:54:33 -0500

    feat: refactor App component to use RouterProvider and add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import {BusTable} from "./components/BusTable.tsx";
+import { Outlet } from "react-router-dom";
+import { Header } from "./components/Header.tsx";
 
 function App() {
     return (
         <div className="min-h-screen bg-gray-50 p-8">
-            <header className="bg-blue-900 px-8 py-4 mb-8 rounded-lg shadow-md">
-                <h1 className="text-white text-2xl font-bold m-0">CIVA - Reto Técnico</h1>
-            </header>
+            <Header />
             <main className="max-w-7xl mx-auto">
-                <BusTable />
+                <Outlet />
             </main>
         </div>
     );
